401828f82c6e59a2522bd42c9a1c69e281b477ea
YAGNESSHETTY <80250245+YAGNESSHETTY@users.noreply.github.com>
2024-03-26 07:25:56 +0000

    variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14443
+let accountName = "Yagnesh"
+var accountPass = "afjsk"
+accountCity = "jaipur"
+let accountState ;
+
+// never use var use let 
+// var has problems in block scope
+// we can use console.table instead of log
+// for multiple values
+console.table([accountId, accountName , accountPass , accountCity , accountState]) ;
